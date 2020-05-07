65e8624402af3ca0059e196589854d46cfdbb127
Parkouras <parkerallen1309@gmail.com>
2020-05-06 17:52:00 -0700

    Created main function

--- Cimplify.py
+++ Cimplify.py
@@ -1,31 +1,41 @@
-import serial
 
-print("Welcome to Cimplify, the solution to Cisco Brainmelt!")
-print(" ")
-print(" ")
-print("[1] Create Router Configuration")
-print(" ")
-print("[2] Create Switch Configuration")
-print(" ")
-print("[3] Exit")
-print(" ")
 
-userSel = input("Select an option:")
+def main():
+    welcome()
+    prompt()
 
-if userSel == "1":
-	print("Router Config Selected!")
-	username = input("Username:")
-	secret = input("Password (encrypted by default):")
-	print("Privilege set to 15 by default.")
-	domainname = input("Domain-Name:")
-	hostname = input("Hostname:")
-	cryptoBits = input("Select key length of 512-2048 for SSH:")
-	print ("username "+username+"privilege 15 secret "+secret)
-	print("conf t")
-	print("ip domain-name "+domainname)
-	print("hostname "+hostname)
-	print("crypto key generate rsa")
-	print(cryptoBits)
+def welcome():
+    print("Welcome to Cimplify, the solution to Cisco Brainmelt!")
+    print(" ")
+    print(" ")
+    print("[1] Create Router Configuration")
+    print(" ")
+    print("[2] Create Switch Configuration")
+    print(" ")
+    print("[3] Exit")
+    print(" ")
+
+def prompt():
+
+    userSel = input("Select an option:")
+
+    if userSel == "1":
+        print("Router Config Selected!")
+        username = input("Username:")
+        secret = input("Password (encrypted by default):")
+        print("Privilege set to 15 by default.")
+        domainname = input("Domain-Name:")
+        hostname = input("Hostname:")
+        cryptoBits = input("Select key length of 512-2048 for SSH:")
+        print ("username "+username+"privilege 15 secret "+secret)
+        print("conf t")
+        print("ip domain-name "+domainname)
+        print("hostname "+hostname)
+        print("crypto key generate rsa")
+        print(cryptoBits)
+        print("copy running-config startup-config")
+
+main()
 
 
 
