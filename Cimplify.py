5d89d6b4ee72bac7e2c960f150a08ecbb7153bef
Parkouras <parkerallen1309@gmail.com>
2020-05-08 04:01:58 -0700

    added command home for different functions

--- Cimplify.py
+++ Cimplify.py
@@ -1,63 +1,116 @@
 import time
-import serial.tools.list_ports
 
 global dataSet
 
 def main():
-    serialConf()
     welcome()
-    prompt()
     
 def welcome():
     print("Welcome to Cimplify, the solution to Cisco Brainmelt!")
     print(" ")
     print(" ")
-    print("[1] Create Router Configuration")
+    print("[1] Create Initial Configuration")
     print(" ")
-    print("[2] Create Switch Configuration")
+    print("[2] Configure other settings")
     print(" ")
     print("[3] Exit")
-    print(" ")
-
-def serialConf():
-    comPorts = list(serial.tools.list_ports.comports())
-    print (comPorts)
-    
-def prompt():
 
     userSel = input("Select an option:")
 
     if userSel == "1":
-        print("Router Config Selected!")
-        username = input("Username:")
-        secret = input("Password (encrypted by default):")
-        print("Privilege set to 15 by default.")
-        domainname = input("Domain-Name:")
-        hostname = input("Hostname:")
-        cryptoBits = input("Select key length of 512-2048 for SSH:")
+        initConf()
+    if userSel == "2":
+        additConf()
 
-        dataSet = [
-            "en",
-            "conf t",
-            "username "+username+" privilege 15 secret "+secret,
-            "ip domain-name "+domainname,
-            "hostname "+hostname,
-            "crypto key generate rsa",
-            cryptoBits,
-            "line vty 0 4",
-            "transport input ssh",
-            "login local"]
-        
-        print("EXPORT THROUGH SERIAL")
-        baud = input("Select Baudrate (Press Enter for default of 9600):")
-        if baud ==(""):
-            baud = (9600)
-        print("Baudrate set to "+str(baud)+("."))
-        cont = input("Make sure you're in global config. Press enter when you are ready.")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(*dataSet, sep = "\n") 
+def initConf():
+
+    print("Router Config Selected!")
+    username = input("Username:")
+    secret = input("Password (encrypted by default):")
+    print("Privilege set to 15 by default.")
+    domainname = input("Domain-Name:")
+    hostname = input("Hostname:")
+    cryptoBits = input("Select key length of 512-2048 for SSH:")
+
+    dataSet = [
+        "en",
+        "conf t",
+        "username "+username+" privilege 15 secret "+secret,
+        "ip domain-name "+domainname,
+        "hostname "+hostname,
+        "crypto key generate rsa",
+        cryptoBits,
+        "line vty 0 4",
+        "transport input ssh",
+        "login local"]
+    
+    print("EXPORT THROUGH SERIAL")
+    baud = input("Select Baudrate (Press Enter for default of 9600):")
+    if baud ==(""):
+        baud = (9600)
+    print("Baudrate set to "+str(baud)+("."))
+    cont = input("Make sure you're in global config. Press enter when you are ready.")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(*dataSet, sep = "\n") 
+    print(" ")
+    userSel = input("Press Q to go home or Enter to continue configuring this device:")
+    if userSel == ("Q"):
+        welcome()
+    if userSel == (""):
+        additConf()
+
+def vlan():
+    portSpeed = 0 
+    userSel = input("Is this a Gigabit, or Fastethernet switch? [G/F]")
+    if userSel == "G":
+        portSpeed = ("GigabitEthernet")
+    if userSel == "F":
+        portSpeed = ("FastEthernet")
+    print("Port set to "+portSpeed+".")
+    vlanID = input("VLAN ID Number: ")
+    vlanName = input("VLAN Name: ")
+    firstPort = input("Select first port in range: ")
+    lastPort = input("Select last port in range: ")
+    confirmGlobal = input("Press Enter when you are in global config.")
+    if confirmGlobal =="":
+        print("en")
+        print("conf t")
+        print("vlan "+vlanID)
+        print("vlan name "+vlanName)
+        print("no shut")
+
+
+
+    
+
+
+def switchConf():
+    print("Switch Configuration")
+    print("Use 'help' for options")
+    print(" ")
+    def command():
+        userSel = input(">")
+        if userSel == ("help"):
+            print("Options are:")
+            print("vlan,    etherchannel,    ")
+            print("")
+            command()
+
+        if userSel == ("vlan"):
+            vlan()
+    command()
+
+def additConf():
+    devSel = input("Additional Configuration for which device?[R]outer/[S]witch: ")
+    print("[R]outer")
+    print(" ")
+    print("[S]witch")
+    if devSel == "R":
+        routerConf()
+    if devSel == "S":
+        switchConf()
 
 main()
 
